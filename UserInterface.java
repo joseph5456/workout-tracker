f614866b64366d152f7d89b236a2321bdb548a3b
joseph <tracy201156@gmail.com>
2025-07-28 17:24:31 -0400

    added option to input number of sets and reps, creating an object and storing it in the list

--- UserInterface.java
+++ UserInterface.java
@@ -19,6 +19,15 @@ public class UserInterface {
             if (readyOrNot.equals("yes")) {
                 System.out.println("First Exercise: ");
                 String exerciseName = scanner.nextLine();
+                System.out.println("Number of sets: ");
+                int sets = Integer.valueOf(scanner.nextLine());
+                System.out.println("Number of reps: ");
+                int reps = Integer.valueOf(scanner.nextLine());
+
+                //create exercise object and add to list
+                Exercise exercise1 = new Exercise(exerciseName, sets, reps);
+                exercises.add(exercise1);
+
             } else if (readyOrNot.equals("no")){
                 System.out.println("You lazy bum!");
                 break;
