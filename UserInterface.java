177a4c5abc1f43a5c4c5db841eeea3e6032c3b7e
joseph <tracy201156@gmail.com>
2025-07-28 17:07:39 -0400

    add scanner and ui object to main, add startup text and yes/no option to start method

--- UserInterface.java
+++ UserInterface.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
+
 public class UserInterface {
+    private Scanner scanner;
 
-
-    public UserInterface() {
-
+    public UserInterface(Scanner scanner) {
+        this.scanner = scanner;
     }
-    
-    public void start() {
 
+    public void start() {
+        while (true) {
+            System.out.println("Welcome back! Ready to start a workout?");
+            System.out.print("Type yes/no: ");
+            String readyOrNot = scanner.nextLine();
+            if (readyOrNot.equals("yes")) {
+                System.out.println("Placeholder");
+            } else if (readyOrNot.equals("no")){
+                break;
+            }
+        }
     }
 }
