3c8cedbe03d7b7c7a6ee15ee4db217e05df30ca1
joseph <tracy201156@gmail.com>
2025-08-01 12:23:03 -0400

    created workout class to store multiple workouts

---
+++ Workout.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Workout {
+    private String workoutName;
+    private ArrayList<String> exercises;
+
+    public Workout() {
+        this.exercises = new ArrayList<>();
+    }
+
+}
