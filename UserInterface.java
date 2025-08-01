c63d3ae8a2f8339030f76f5ffa1f64b65cfedd47
joseph <tracy201156@gmail.com>
2025-08-01 13:17:37 -0400

    restructured UserInterface class, created logWorkout and printSummary methods to make code look cleaner

--- UserInterface.java
+++ UserInterface.java
@@ -3,71 +3,81 @@ import java.util.Scanner;
 
 public class UserInterface {
     private Scanner scanner;
-    private ArrayList<Exercise> exercises;
+    //private ArrayList<Exercise> exercises;
+    private ArrayList<Workout> workouts;
+    private Workout workout;
 
     public UserInterface(Scanner scanner) {
         this.scanner = scanner;
-        this.exercises = new ArrayList<>();
+        //this.exercises = new ArrayList<>();
+        this.workouts = new ArrayList<>();
     }
 
     public void start() {
         System.out.println("Welcome back! Ready to start a workout?");
         System.out.print("Type yes/no: ");
+        String input = scanner.nextLine();
+
+        if (input.equals("yes")) {
+            logWorkout();
+            printSummary();
+        } else if (input.equals("no")) {
+            System.out.println("You lazy bum!");
+        }
+    }
+
+    public void logWorkout() {
+        System.out.println("Workout name: " );
+        String workoutName = scanner.nextLine();
+        this.workout = new Workout(workoutName);
+
+        int exerciseNum = 1;
 
         while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("yes")) {
-                int exerciseNum = 1;
-
-                while (true) {
-                    System.out.println("\nExercise " + exerciseNum + ":");
-                    String exerciseName = scanner.nextLine();
-                    if (exerciseName.equals("stop")) {
-                        break;
-                    }
-                    System.out.println("Number of sets: ");
-                    int sets = Integer.parseInt(scanner.nextLine());
-
-                    int maxReps = 0;
-                    int maxWeight = 0;
-                    for (int i = 1; i <= sets; i++) {
-                        System.out.print("Please enter weight for set " + i + ": ");
-                        int weight = Integer.parseInt(scanner.nextLine());
-                        if (weight > maxWeight) {
-                            maxWeight = weight;
-                        }
-
-                        System.out.print("Please enter number of reps for set " + i + ": ");
-                        int reps = Integer.parseInt(scanner.nextLine());
-                        if (reps > maxReps) {
-                            maxReps = reps;
-                        }
-                    }
-
-                    //create exercise object and add to list
-                    Exercise exercise = new Exercise(exerciseName, sets, maxReps, maxWeight);
-                    exercises.add(exercise);
-                    exerciseNum++;
-                }
-                break;
-
-            } else if (input.equals("no")){
-                System.out.println("You lazy bum!");
+            System.out.println("\nExercise " + exerciseNum + ":");
+            String exerciseName = scanner.nextLine();
+            if (exerciseName.equals("stop")) {
                 break;
             }
-        }
+            System.out.println("Number of sets: ");
+            int sets = Integer.parseInt(scanner.nextLine());
 
-        if (!exercises.isEmpty()) {
+            int maxReps = 0;
+            int maxWeight = 0;
+            for (int i = 1; i <= sets; i++) {
+                System.out.print("Please enter weight for set " + i + ": ");
+                int weight = Integer.parseInt(scanner.nextLine());
+                if (weight > maxWeight) {
+                    maxWeight = weight;
+                }
+
+                System.out.print("Please enter number of reps for set " + i + ": ");
+                int reps = Integer.parseInt(scanner.nextLine());
+                if (reps > maxReps) {
+                    maxReps = reps;
+                }
+            }
+
+            //create exercise object and add to list
+            Exercise exercise = new Exercise(exerciseName, sets, maxReps, maxWeight);
+            workout.addExercise(exercise);
+            exerciseNum++;
+        }
+    }
+
+    public void printSummary() {
+        if (workout != null && !this.workout.getExercises().isEmpty()) {
             System.out.println("\nSummary: \n-------------------------");
-            for (Exercise exercise : exercises) {
+            for (Exercise exercise : workout.getExercises()) {
                 System.out.println(exercise);
             }
 
             int totalVolume = 0;
-            for (Exercise exercise : exercises) {
+            for (Exercise exercise : workout.getExercises()) {
                 totalVolume += exercise.getVolume();
             }
             System.out.println("Total volume lifted: " + totalVolume + " lbs");
         }
     }
+
 }
