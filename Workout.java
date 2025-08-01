ee760ce0a0494da667e1bfe9017771637f694dff
joseph <tracy201156@gmail.com>
2025-08-01 13:16:43 -0400

    created methods to add exercises and return exercise list

--- Workout.java
+++ Workout.java
@@ -2,10 +2,19 @@ import java.util.ArrayList;
 
 public class Workout {
     private String workoutName;
-    private ArrayList<String> exercises;
+    private ArrayList<Exercise> exercises;
 
-    public Workout() {
+    public Workout(String workoutName) {
+        this.workoutName = workoutName;
         this.exercises = new ArrayList<>();
+    }
+
+    public ArrayList<Exercise> getExercises() {
+        return this.exercises;
+    }
+
+    public void addExercise(Exercise exercise) {
+        exercises.add(exercise);
     }
 
 }
