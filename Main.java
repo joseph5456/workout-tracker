0c463ce2379ec0879c29597205be382cfe827c86
joseph <tracy201156@gmail.com>
2025-08-13 13:38:23 -0400

    added I/O reader to test

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            writer.write("Writing to a file.");
+            writer.write("\nAnother line");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         UserInterface ui = new UserInterface(scanner);
+
 
         ui.start();
     }
