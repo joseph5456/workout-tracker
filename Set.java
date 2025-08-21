a1a518c5afd296ff6f50f6e311f607760e947c38
joseph <tracy201156@gmail.com>
2025-08-21 15:40:32 -0400

    created set class to store weight and reps for each set, making it easier to calculate volume for each workout

---
+++ Set.java
@@ -0,0 +1,17 @@
+public class Set {
+    private double weight;
+    private int reps;
+
+    public Set(double weight, int reps) {
+        this.weight = weight;
+        this.reps = reps;
+    }
+
+    public double getVolume() {
+        return weight * reps;
+    }
+
+    public String toString() {
+        return weight + " lbs" + reps + " reps";
+    }
+}
