9fc74b894416562770882ca96897c8efbd6c368f
joseph <tracy201156@gmail.com>
2025-08-22 14:39:37 -0400

    added FileManager class with methods to read and store workouts

---
+++ FileManager.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class FileManager {
+
+    public FileManager() {
+
+    }
+
+    public void saveWorkout(String workout) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("workouts.txt"));
+            writer.write(workout + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadWorkout() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("workouts.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println("Loaded: " + line);
+            }
+        } catch (IOException e ) {
+            e.printStackTrace();
+        }
+    }
+}
