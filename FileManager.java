648ef29a95ea194d9dc5664e01bbb84f2141f3f1
joseph <tracy201156@gmail.com>
2025-08-23 13:22:35 -0400

    configured loadWorkout method to return a list of the workouts instead of printing it one by one. Created clearWorkouts method to clear workouts if needed

--- FileManager.java
+++ FileManager.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class FileManager {
 
@@ -11,13 +13,27 @@ public class FileManager {
         }
     }
 
-    public void loadWorkout() {
+    //Load workouts into a list
+    public ArrayList<String> loadWorkout() {
+        ArrayList<String> workouts = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader("workouts.txt"))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                System.out.println("Loaded: " + line);
+                workouts.add(line);
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("No workouts found");
         } catch (IOException e ) {
+            e.printStackTrace();
+        }
+        return workouts;
+    }
+
+    //clear all workouts
+    public void clearWorkouts() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("workouts.txt", false))) {
+            //writing nothing resets the file
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
